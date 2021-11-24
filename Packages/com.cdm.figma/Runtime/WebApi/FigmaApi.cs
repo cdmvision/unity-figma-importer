de94c0b466646edb871f1e19888e6f9136386537
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2021-11-25 02:50:16 +0300

    Minor changes

--- Packages/com.cdm.figma/Runtime/WebApi/FigmaApi.cs
+++ Packages/com.cdm.figma/Runtime/WebApi/FigmaApi.cs
@@ -11,6 +11,98 @@ namespace Cdm.Figma
     {
         private const string BaseUri = "https://api.figma.com/v1";
 
+        
+
+        /// <summary>
+        /// Returns the document referred to by :key as a JSON object string. The file key can be parsed from any
+        /// Figma file url: https://www.figma.com/file/:key/:title
+        ///
+        /// The components key contains a mapping from node IDs to component metadata. This is to help you
+        /// determine which components each instance comes from.
+        /// </summary>
+        public static async Task<string> GetFileAsTextAsync(FigmaFileRequest fileRequest)
+        {
+            if (string.IsNullOrEmpty(fileRequest.personalAccessToken))
+                throw new ArgumentException("Personal access token cannot be empty.");
+
+            if (string.IsNullOrEmpty(fileRequest.fileId))
+                throw new ArgumentException("File ID cannot be empty.");
+
+            var uri = GetFileRequestUrl(fileRequest);
+            var result = await GetContentAsync(uri, fileRequest.personalAccessToken);
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the document referred to by :key as a <see cref="FigmaFile"/>.
+        /// </summary>
+        /// <seealso cref="GetFileAsTextAsync"/>
+        public static async Task<FigmaFile> GetFileAsync(FigmaFileRequest fileRequest)
+        {
+            var result = await GetFileAsTextAsync(fileRequest);
+            return FigmaFile.FromString(result);
+        }
+        
+        /// <summary>
+        /// Renders images from a file.
+        /// 
+        /// If no error occurs, "images" will be populated with a map from node IDs to URLs of the rendered images,
+        /// and "status" will be omitted. The image assets will expire after 30 days.
+        /// 
+        /// Important: the image map may contain values that are null. This indicates that rendering of that specific
+        /// node has failed. This may be due to the node id not existing, or other reasons such has the node having no
+        /// renderable components. It is guaranteed that any node that was requested for rendering will be represented
+        /// in this map whether or not the render succeeded.
+        /// </summary>
+        public static async Task<Dictionary<string, byte[]>> GetImageAsync(FigmaImageRequest imageRequest)
+        {
+            if (string.IsNullOrEmpty(imageRequest.personalAccessToken))
+                throw new ArgumentException("Personal access token cannot be empty.");
+
+            if (string.IsNullOrEmpty(imageRequest.fileId))
+                throw new ArgumentException("File ID cannot be empty.");
+            
+            if (imageRequest.ids == null || imageRequest.ids.Length == 0)
+                throw new ArgumentException("Image ids array must has at least one item.");
+            
+            // Get image download URLs.
+            var uri = GetImageRequestUrl(imageRequest);
+            var result = await GetContentAsync(uri, imageRequest.personalAccessToken);
+
+            var response = JsonConvert.DeserializeObject<FigmaImageResponse>(result);
+            if (response == null)
+                throw new Exception("Cannot get images. Response is null.");
+
+            if (!string.IsNullOrEmpty(response.error))
+                throw new Exception(response.error);
+
+            // Download images data.
+            var images = new Dictionary<string, byte[]>();
+
+            foreach (var (imageId, imageUrl) in response.images)
+            {
+                if (!string.IsNullOrEmpty(imageUrl))
+                {
+                    var imageData = await GetBytesAsync(imageUrl);
+                    if (imageData == null)
+                        throw new Exception($"Image '{imageId}' data could not be get from: {imageUrl}");
+                    
+                    images.Add(imageId, imageData);
+                }
+                else
+                {
+                    images.Add(imageId, null);
+                }
+            }
+
+            return images;
+        }
+
+        public static async Task<byte[]> GetThumbnailImageAsync(string thumbnailUrl)
+        {
+            return await GetBytesAsync(thumbnailUrl, "image/png");
+        }
+        
         private static string GetFileRequestUrl(FigmaFileRequest request)
         {
             var url = $"{BaseUri}/files/{request.fileId}";
@@ -61,67 +153,6 @@ namespace Cdm.Figma
             url = $"{url}&svg_simplify_stroke={request.svgSimplifyStroke}";
             url = $"{url}&use_absolute_bounds={request.useAbsoluteBounds}";
             return url;
-        }
-
-        public static async Task<string> GetFileAsTextAsync(FigmaFileRequest fileRequest)
-        {
-            if (string.IsNullOrEmpty(fileRequest.personalAccessToken))
-                throw new ArgumentException("Personal access token cannot be empty.");
-
-            if (string.IsNullOrEmpty(fileRequest.fileId))
-                throw new ArgumentException("File ID cannot be empty.");
-
-            var uri = GetFileRequestUrl(fileRequest);
-            var result = await GetContentAsync(uri, fileRequest.personalAccessToken);
-            return result;
-        }
-
-        public static async Task<FigmaFile> GetFileAsync(FigmaFileRequest fileRequest)
-        {
-            var result = await GetFileAsTextAsync(fileRequest);
-            return FigmaFile.FromString(result);
-        }
-        
-        public static async Task<Dictionary<string, byte[]>> GetImageAsync(FigmaImageRequest imageRequest)
-        {
-            if (string.IsNullOrEmpty(imageRequest.personalAccessToken))
-                throw new ArgumentException("Personal access token cannot be empty.");
-
-            if (string.IsNullOrEmpty(imageRequest.fileId))
-                throw new ArgumentException("File ID cannot be empty.");
-            
-            if (imageRequest.ids == null || imageRequest.ids.Length == 0)
-                throw new ArgumentException("Image ids array must has at least one item.");
-            
-            // Get image download URLs.
-            var uri = GetImageRequestUrl(imageRequest);
-            var result = await GetContentAsync(uri, imageRequest.personalAccessToken);
-
-            var response = JsonConvert.DeserializeObject<FigmaImageResponse>(result);
-            if (response == null)
-                throw new Exception("Cannot get images. Response is null.");
-
-            if (!string.IsNullOrEmpty(response.error))
-                throw new Exception(response.error);
-
-            // Download images data.
-            var images = new Dictionary<string, byte[]>();
-
-            foreach (var (imageId, imageUrl) in response.images)
-            {
-                var imageData = await GetBytesAsync(imageUrl);
-                if (imageData == null)
-                    throw new Exception($"Image '{imageId}' data could not be get from: {imageUrl}");
-                
-                images.Add(imageId, imageData);
-            }
-
-            return images;
-        }
-
-        public static async Task<byte[]> GetThumbnailImageAsync(string thumbnailUrl)
-        {
-            return await GetBytesAsync(thumbnailUrl, "image/png");
         }
         
         private static async Task<byte[]> GetBytesAsync(string url, string contentType = "")
