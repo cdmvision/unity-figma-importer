92bb51ab30c1fe4f116842b7ac6529567c75b4a6
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2021-11-25 19:58:13 +0300

    Minor changes

--- Packages/com.cdm.figma/Runtime/IFigmaImporter.cs
+++ Packages/com.cdm.figma/Runtime/IFigmaImporter.cs
@@ -4,11 +4,19 @@ namespace Cdm.Figma
 {
     public interface IFigmaImporter
     {
+        /// <summary>
+        /// Imports pages and all their nodes from Figma file given into Unity.
+        /// </summary>
+        /// <param name="file">Figma file to be imported.</param>
+        /// <param name="options">Optional import options.</param>
         Task ImportFileAsync(FigmaFile file, FigmaImportOptions options = null);
     }
     
     public class FigmaImportOptions
     {
+        /// <summary>
+        /// Set page IDs to import only selected pages. Leave as <c>null</c> to import all pages.
+        /// </summary>
         public string[] pages;
     }
 }
