2c482ac22b410046e21a669c5fc9a7e186961a05
gunseli <gns.ozkan@gmail.com>
2021-11-26 15:57:38 +0300

    test rectangle_node converter

--- Packages/com.cdm.figma/Runtime/UIToolkit/RectangleNodeConverter.cs
+++ Packages/com.cdm.figma/Runtime/UIToolkit/RectangleNodeConverter.cs
@@ -6,11 +6,13 @@ namespace Cdm.Figma.UIToolkit
     [CreateAssetMenu(fileName = nameof(RectangleNodeConverter), menuName = AssetMenuRoot + "Rectangle", order = 20)]
     public class RectangleNodeConverter : NodeConverter<RectangleNode>
     {
+        private const string xmlType = "ui:VisualElement";
         public override XElement Convert(FigmaImporter importer, FigmaFile file, Node node)
         {
             var rectangleNode = (RectangleNode) node;
-            // TODO: Implement
-            throw new System.NotImplementedException();
+            var rectangleElement = new XElement(xmlType, new XAttribute("name", rectangleNode.name));
+            Debug.Log($"Rectangle XML element: {rectangleElement}");            
+            return rectangleElement;
         }
     }
 }
