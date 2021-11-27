5160dfca66c0d24c341d73603c36ed197f0363de
gunseli <gns.ozkan@gmail.com>
2021-11-27 15:18:30 +0300

    generate basic rectangle node
    size, positioning, corner shaping, and colors are generated.

--- Packages/com.cdm.figma/Runtime/UIToolkit/RectangleNodeConverter.cs
+++ Packages/com.cdm.figma/Runtime/UIToolkit/RectangleNodeConverter.cs
@@ -10,11 +10,76 @@ namespace Cdm.Figma.UIToolkit
         public override XElement Convert(Node node, NodeConvertArgs args)
         {
             var rectangleNode = (RectangleNode) node;
-            var rectangleElement = 
-                new XElement(args.namespaces.engine + nameof(VisualElement), 
-                    new XAttribute("name", rectangleNode.name));
-            Debug.Log($"Rectangle XML element: {rectangleElement}");            
+            var styleAttributes = BuildStyle(rectangleNode);
+            var rectangleElement =
+                new XElement(args.namespaces.engine + nameof(VisualElement),
+                    new XAttribute("name", rectangleNode.name),
+                    new XAttribute("style", styleAttributes));
+
+            Debug.Log($"Rectangle XML element: {rectangleElement}");
             return rectangleElement;
+        }
+
+        private string BuildStyle(RectangleNode node)
+        {
+            string styleAttributes = "";
+            
+            /*positioning and size*/
+            var absoluteBoundingBox = node.absoluteBoundingBox;
+            var width = absoluteBoundingBox.width;
+            styleAttributes += "width: " + width + "px; ";
+            var height = absoluteBoundingBox.height;
+            styleAttributes += "height: " + height + "px; ";
+            
+            var xValue = absoluteBoundingBox.x;
+            var yValue = absoluteBoundingBox.y;
+            var constraints = node.constraints;
+            var constraintX = constraints.horizontal;
+            //Constraints are stored as "Top" or "Left" etc..
+            //Using toLower because uxml expects "top" or "left" etc...
+            styleAttributes += constraintX.ToString().ToLower() + ": " + xValue + "px; ";   
+            var constraintY = constraints.vertical;
+            styleAttributes += constraintY.ToString().ToLower() + ": " + yValue + "px; ";
+            /*positioning and size*/
+            
+            /*shaping*/
+            var cornerRadius = node.cornerRadius;
+            var cornerRadii = node.rectangleCornerRadii;
+            //If element has 4 different radius for each corner, use this
+            if (cornerRadii != null)
+            {
+                styleAttributes += "border-radius: ";
+                styleAttributes += cornerRadii[0] + "px " + 
+                                   cornerRadii[1] + "px " + 
+                                   cornerRadii[2] + "px " + 
+                                   cornerRadii[3] + "px; "; 
+            }
+            //Element does not have 4 different radius for each corner...
+            else
+            {
+                //Does it have cornerRadius?
+                if (cornerRadius.HasValue)
+                {
+                    styleAttributes += "border-radius: " + cornerRadius + "px; ";
+                }
+            }
+            /*shaping*/
+            
+            /*color*/
+            var fills = node.fills;
+            foreach (var fill in fills)
+            {
+                //Probably we will use only one color (for background color), but it can change in the future,
+                //If fills have more than one fill color, then Blend Type must be checked. 
+                //TODO: Implement when fills have more than one fill color.
+                styleAttributes += "background-color: rgba(";
+                var bgColor = fill.color;
+                //Figma stores rgb values in [0,1], change it to [0,255] (except alpha).
+                styleAttributes += bgColor.r*255 + "," + bgColor.g*255 + "," + bgColor.b*255 + "," + bgColor.a + ");";
+            }
+            /*color*/
+ 
+            return styleAttributes;
         }
     }
 }
