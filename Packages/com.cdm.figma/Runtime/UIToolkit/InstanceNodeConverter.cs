0b22209f24526db786155d05398009ca5f298381
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2021-12-04 20:02:37 +0300

    Minor changes

--- Packages/com.cdm.figma/Runtime/UIToolkit/InstanceNodeConverter.cs
+++ Packages/com.cdm.figma/Runtime/UIToolkit/InstanceNodeConverter.cs
@@ -4,11 +4,28 @@ namespace Cdm.Figma.UIToolkit.UIToolkit
 {
     public class InstanceNodeConverter : NodeConverter<InstanceNode>
     {
+        public override bool CanConvert(Node node, NodeConvertArgs args)
+        {
+            if (!base.CanConvert(node, args))
+                return false;
+            
+            var instanceNode = (InstanceNode) node;
+            if (instanceNode.mainComponent == null)
+                return false;
+
+            if (instanceNode.mainComponent.componentSet != null)
+                return false;
+
+            return true;
+        }
+
         public override NodeElement Convert(Node node, NodeConvertArgs args)
         {
             var instanceNode = (InstanceNode) node;
 
             var element = NodeElement.New<VisualElement>(instanceNode, args);
+            
+            // TODO: Call FrameNodeConverter
             
             var children = instanceNode.children;
             if (children != null)
