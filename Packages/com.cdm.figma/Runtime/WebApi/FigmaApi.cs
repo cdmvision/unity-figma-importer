bfcbd628582d8b5deb6432d1ea2d6feaff8e2b63
H. Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2021-12-10 04:38:32 +0300

    Update project to 2022.1b

--- Packages/com.cdm.figma/Runtime/WebApi/FigmaApi.cs
+++ Packages/com.cdm.figma/Runtime/WebApi/FigmaApi.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
-using Unity.Plastic.Newtonsoft.Json;
+using Newtonsoft.Json;
 using UnityEngine;
 
 namespace Cdm.Figma
--- Packages/com.cdm.figma/Runtime/WebApi/FigmaImageRequest.cs
+++ Packages/com.cdm.figma/Runtime/WebApi/FigmaImageRequest.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Unity.Plastic.Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace Cdm.Figma
 {
