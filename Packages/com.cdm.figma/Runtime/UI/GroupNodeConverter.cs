74c1e7cfb0795d3f1d46e197a282f512324d6163
gunseli <gns.ozkan@gmail.com>
2022-02-13 21:01:43 +0300

    handle auto (vertical-horizontal) layout properties

--- Packages/com.cdm.figma/Runtime/UI/GroupNodeConverter.cs
+++ Packages/com.cdm.figma/Runtime/UI/GroupNodeConverter.cs
@@ -1,3 +1,4 @@
+using System;
 using Unity.VectorGraphics;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,7 +13,6 @@ namespace Cdm.Figma.UI
             
             SetTransformOrigin(groupNodeObject);
             SetSize(groupNode, groupNodeObject);
-            SetPosition(groupNode, groupNodeObject);
             SetRotation(groupNode, groupNodeObject);
             HandleConstraints(groupNodeObject, groupNode);
             
@@ -24,8 +24,9 @@ namespace Cdm.Figma.UI
 
             if (groupNode.layoutMode != LayoutMode.None)
             {
-                HandleAutoLayout(groupNode, groupNodeObject);
-                //TODO: Handle Axis Sizing
+                AddLayoutComponent(groupNode, groupNodeObject);
+                AddPadding(groupNode, groupNodeObject);
+                AddContentSizeFitter(groupNode, groupNodeObject);
             }
 
             var children = groupNode.children;
@@ -38,7 +39,7 @@ namespace Cdm.Figma.UI
                         if (groupNode.layoutMode != LayoutMode.None)
                         {
                             childElement.gameObject.AddComponent<LayoutElement>();
-                            //TODO: Handle resizing
+                            HandleFillContainer(groupNode.layoutMode, groupNodeObject, childElement);
                         }
                         
                         if (childElement != groupNodeObject)
@@ -48,59 +49,242 @@ namespace Cdm.Figma.UI
                     }
                 }
             }
-
+            
+            SetPosition(groupNode, groupNodeObject);
             return groupNodeObject;
         }
-        
+
         public override NodeObject Convert(NodeObject parentObject, Node node, NodeConvertArgs args)
         {
             return Convert(parentObject, (GroupNode) node, args);
         }
+        
+        private static void HandleFillContainer(LayoutMode groupNodeLayoutMode, NodeObject groupNodeObject, NodeObject childElement)
+        {
+            INodeLayout childLayout = (INodeLayout) childElement.node;
+            INodeTransform childTransform = (INodeTransform) childElement.node;
+            if (childLayout.layoutAlign == LayoutAlign.Stretch)
+            {
+                if (groupNodeLayoutMode == LayoutMode.Horizontal)
+                {
+                    groupNodeObject.GetComponent<HorizontalLayoutGroup>().childControlHeight = true;
+                    childElement.gameObject.GetComponent<LayoutElement>().flexibleHeight = 1;
+                }
+                else if (groupNodeLayoutMode == LayoutMode.Vertical)
+                {
+                    groupNodeObject.GetComponent<VerticalLayoutGroup>().childControlWidth = true;
+                    childElement.gameObject.GetComponent<LayoutElement>().flexibleWidth = 1;
+                }
+            }
+            else
+            {
+                if (groupNodeLayoutMode == LayoutMode.Horizontal)
+                {
+                    childElement.gameObject.GetComponent<LayoutElement>().preferredHeight = childTransform.size.y;
+                }
+                else
+                {
+                    childElement.gameObject.GetComponent<LayoutElement>().preferredWidth = childTransform.size.x;
+                }
+                
+            }
 
-        private static void HandleAutoLayout(GroupNode groupNode, NodeObject groupNodeObject)
+            if (childLayout.layoutGrow.HasValue && childLayout.layoutGrow!=0)
+            {
+                if (groupNodeLayoutMode == LayoutMode.Horizontal)
+                {
+                    groupNodeObject.GetComponent<HorizontalLayoutGroup>().childControlWidth = true;
+                    childElement.gameObject.GetComponent<LayoutElement>().flexibleWidth = 1;
+                }
+                else if (groupNodeLayoutMode == LayoutMode.Vertical)
+                {
+                    groupNodeObject.GetComponent<VerticalLayoutGroup>().childControlHeight = true;
+                    childElement.gameObject.GetComponent<LayoutElement>().flexibleHeight = 1;
+                }
+            }
+            else
+            {
+                if (groupNodeLayoutMode == LayoutMode.Horizontal)
+                {
+                    childElement.gameObject.GetComponent<LayoutElement>().preferredWidth = childTransform.size.x;
+                }
+                else
+                {
+                    childElement.gameObject.GetComponent<LayoutElement>().preferredHeight = childTransform.size.y;
+                }
+            }
+        }
+
+        private static void AddContentSizeFitter(GroupNode groupNode, NodeObject groupNodeObject)
+        {
+            if (groupNode.primaryAxisSizingMode == AxisSizingMode.Auto || groupNode.counterAxisSizingMode == AxisSizingMode.Auto)
+            {
+                groupNodeObject.gameObject.AddComponent<ContentSizeFitter>();
+            }
+
+            if (groupNode.primaryAxisSizingMode == AxisSizingMode.Auto)
+            {
+                if (groupNode.layoutMode == LayoutMode.Horizontal)
+                {
+                    groupNodeObject.gameObject.GetComponent<ContentSizeFitter>().horizontalFit =
+                        ContentSizeFitter.FitMode.PreferredSize;
+                }
+                else
+                {
+                    groupNodeObject.gameObject.GetComponent<ContentSizeFitter>().verticalFit =
+                        ContentSizeFitter.FitMode.PreferredSize;
+                }
+            }
+
+            if (groupNode.counterAxisSizingMode == AxisSizingMode.Auto)
+            {
+                if (groupNode.layoutMode == LayoutMode.Horizontal)
+                {
+                    groupNodeObject.gameObject.GetComponent<ContentSizeFitter>().verticalFit =
+                        ContentSizeFitter.FitMode.PreferredSize;
+                }
+                else
+                {
+                    groupNodeObject.gameObject.GetComponent<ContentSizeFitter>().horizontalFit =
+                        ContentSizeFitter.FitMode.PreferredSize;
+                }
+            }
+        }
+
+        private static void AddPadding(GroupNode groupNode, NodeObject groupNodeObject)
+        {
+            if (groupNode.layoutMode == LayoutMode.Horizontal)
+            {
+                groupNodeObject.GetComponent<HorizontalLayoutGroup>().padding = new RectOffset((int)groupNode.paddingLeft,
+                    (int)groupNode.paddingRight, (int)groupNode.paddingTop, (int)groupNode.paddingBottom);
+            }
+            else
+            {
+                groupNodeObject.GetComponent<VerticalLayoutGroup>().padding = new RectOffset((int)groupNode.paddingLeft,
+                    (int)groupNode.paddingRight, (int)groupNode.paddingTop, (int)groupNode.paddingBottom);
+            }
+        }
+        
+        private static void AddLayoutComponent(GroupNode groupNode, NodeObject groupNodeObject)
         {
             var layoutMode = groupNode.layoutMode;
             if (layoutMode == LayoutMode.Horizontal)
             {
                 groupNodeObject.gameObject.AddComponent<HorizontalLayoutGroup>();
+                HorizontalLayoutGroup layoutGroup = groupNodeObject.gameObject.GetComponent<HorizontalLayoutGroup>();
+                layoutGroup.childControlWidth = false;
+                layoutGroup.childControlHeight = false;
+                layoutGroup.childScaleWidth = false;
+                layoutGroup.childScaleHeight = false;
+                layoutGroup.childForceExpandWidth = false;
+                layoutGroup.childForceExpandHeight = false;
+                if (groupNode.primaryAxisAlignItems == PrimaryAxisAlignItems.Min)
+                {
+                    if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Min)
+                    {
+                        layoutGroup.childAlignment = TextAnchor.UpperLeft;
+                    }
+                    else if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Max)
+                    {
+                        layoutGroup.childAlignment = TextAnchor.LowerLeft;
+                    }
+                    else if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Center)
+                    {
+                        layoutGroup.childAlignment = TextAnchor.MiddleLeft;
+                    }
+                }
+                else if (groupNode.primaryAxisAlignItems == PrimaryAxisAlignItems.Max)
+                {
+                    if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Min)
+                    {
+                        layoutGroup.childAlignment = TextAnchor.UpperRight;
+                    }
+                    else if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Max)
+                    {
+                        layoutGroup.childAlignment = TextAnchor.LowerRight;
+                    }
+                    else if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Center)
+                    {
+                        layoutGroup.childAlignment = TextAnchor.MiddleRight;
+                    }
+                }
+                else if (groupNode.primaryAxisAlignItems == PrimaryAxisAlignItems.Center)
+                {
+                    if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Min)
+                    {
+                        layoutGroup.childAlignment = TextAnchor.UpperCenter;
+                    }
+                    else if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Max)
+                    {
+                        layoutGroup.childAlignment = TextAnchor.LowerCenter;
+                    }
+                    else if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Center)
+                    {
+                        layoutGroup.childAlignment = TextAnchor.MiddleCenter;
+                    }
+                }
+
+                layoutGroup.spacing = groupNode.itemSpacing;
             }
             else
             {
                 groupNodeObject.gameObject.AddComponent<VerticalLayoutGroup>();
+                VerticalLayoutGroup layoutGroup = groupNodeObject.gameObject.GetComponent<VerticalLayoutGroup>();
+                layoutGroup.childControlWidth = false;
+                layoutGroup.childControlHeight = false;
+                layoutGroup.childScaleWidth = false;
+                layoutGroup.childScaleHeight = false;
+                layoutGroup.childForceExpandWidth = false;
+                layoutGroup.childForceExpandHeight = false;
+                if (groupNode.primaryAxisAlignItems == PrimaryAxisAlignItems.Min)
+                {
+                    if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Min)
+                    {
+                        layoutGroup.childAlignment = TextAnchor.UpperLeft;
+                    }
+                    else if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Max)
+                    {
+                        layoutGroup.childAlignment = TextAnchor.UpperRight;
+                    }
+                    else if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Center)
+                    {
+                        layoutGroup.childAlignment = TextAnchor.UpperCenter;
+                    }
+                }
+                else if (groupNode.primaryAxisAlignItems == PrimaryAxisAlignItems.Max)
+                {
+                    if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Min)
+                    {
+                        layoutGroup.childAlignment = TextAnchor.LowerLeft;
+                    }
+                    else if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Max)
+                    {
+                        layoutGroup.childAlignment = TextAnchor.LowerRight;
+                    }
+                    else if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Center)
+                    {
+                        layoutGroup.childAlignment = TextAnchor.LowerCenter;
+                    }
+                }
+                else if (groupNode.primaryAxisAlignItems == PrimaryAxisAlignItems.Center)
+                {
+                    if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Min)
+                    {
+                        layoutGroup.childAlignment = TextAnchor.MiddleLeft;
+                    }
+                    else if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Max)
+                    {
+                        layoutGroup.childAlignment = TextAnchor.MiddleRight;
+                    }
+                    else if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Center)
+                    {
+                        layoutGroup.childAlignment = TextAnchor.MiddleCenter;
+                    }
+                }
+
+                layoutGroup.spacing = groupNode.itemSpacing;
             }
             
-            //TODO: FINISH UP AUTO LAYOUT 
-            /*
-            if (groupNode.primaryAxisAlignItems == PrimaryAxisAlignItems.Min)
-            {
-                
-            }
-            else if (groupNode.primaryAxisAlignItems == PrimaryAxisAlignItems.Max)
-            {
-                
-            }
-            else if (groupNode.primaryAxisAlignItems == PrimaryAxisAlignItems.Center)
-            {
-                
-            }
-            else if (groupNode.primaryAxisAlignItems == PrimaryAxisAlignItems.SpaceBetween)
-            {
-                
-            }
-
-            if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Min)
-            {
-                
-            }
-            else if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Max)
-            {
-                
-            }
-            else if (groupNode.counterAxisAlignItems == CounterAxisAlignItems.Center)
-            {
-                
-            }
-            */
         }
 
         private static void SetPosition(GroupNode groupNode, NodeObject groupNodeObject)
