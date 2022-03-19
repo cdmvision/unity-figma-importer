5b561b0fa6ecd0ecc157e2f300ce8c52b336fd53
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2022-03-19 17:06:02 +0300

    Fix rotation/scaling of figma node

---
+++ Packages/com.cdm.figma.ui/Runtime/NodeObjectExtensions.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+namespace Cdm.Figma.UI
+{
+    public static class NodeObjectExtensions
+    {
+        /// <summary>
+        /// Sets pivot, position, rotation and scale using <paramref name="nodeTransform"/>.
+        /// </summary>
+        /// <seealso cref="SetPivot"/>
+        /// <seealso cref="SetPosition"/>
+        /// <seealso cref="SetRotation"/>
+        /// <seealso cref="SetScale"/>
+        public static void SetTransform(this NodeObject nodeObject, INodeTransform nodeTransform)
+        {
+            nodeObject.SetPivot();
+            nodeObject.SetPosition(nodeTransform);
+            nodeObject.SetRotation(nodeTransform);
+            nodeObject.SetScale(nodeTransform);
+        }
+
+        /// <summary>
+        /// Sets the pivot of the figma node.
+        /// </summary>
+        public static void SetPivot(this NodeObject nodeObject)
+        {
+            // Pivot location is located at top left corner in Figma.
+            nodeObject.rectTransform.pivot = new Vector2(0f, 1f);
+        }
+        
+        /// <summary>
+        /// Sets the position of the figma node.
+        /// </summary>
+        public static void SetPosition(this NodeObject nodeObject, INodeTransform nodeTransform)
+        {
+            nodeObject.rectTransform.position = nodeTransform.relativeTransform.GetPosition();
+        }
+        
+        /// <summary>
+        /// Sets the rotation of the figma node.
+        /// </summary>
+        public static void SetRotation(this NodeObject nodeObject, INodeTransform nodeTransform)
+        {
+            nodeObject.rectTransform.rotation = nodeTransform.relativeTransform.GetRotation();
+        }
+        
+        /// <summary>
+        /// Sets the scale of the figma node.
+        /// </summary>
+        /// <remarks>
+        /// In Figma, there is no 'scaling'. But vertical/horizontal mirror information is stored as scale in
+        /// <see cref="INodeTransform.relativeTransform"/>.
+        /// </remarks>
+        public static void SetScale(this NodeObject nodeObject, INodeTransform nodeTransform)
+        {
+            var scale = nodeTransform.relativeTransform.GetScale();
+            nodeObject.rectTransform.localScale = new Vector3(scale.x, scale.y, 1);
+        }
+        
+        /// <summary>
+        /// Sets the size of the figma node.
+        /// </summary>
+        public static void SetSize(this NodeObject nodeObject, INodeTransform nodeTransform)
+        {
+            nodeObject.rectTransform.sizeDelta = new Vector2(nodeTransform.size.x, nodeTransform.size.y);
+        }
+    }
+}
--- Packages/com.cdm.figma.ui/Runtime/VectorNodeConverter.cs
+++ Packages/com.cdm.figma.ui/Runtime/VectorNodeConverter.cs
@@ -10,11 +10,9 @@ namespace Cdm.Figma.UI
         public static NodeObject Convert(NodeObject parentObject, VectorNode vectorNode, NodeConvertArgs args)
         {
             var nodeObject = NodeObject.NewNodeObject(vectorNode, args);
-            SetTransformOrigin(nodeObject);
-            SetSize(vectorNode, nodeObject);
-            SetRotation(vectorNode, nodeObject);
-            SetPosition(vectorNode, nodeObject);
 
+            nodeObject.SetTransform(vectorNode);
+            nodeObject.SetSize(vectorNode);
             if ((vectorNode.fills.Count > 0 || vectorNode.strokes.Any()) && vectorNode.type != NodeType.Text)
             {
                 var sprite = VectorImageUtils.CreateSprite(vectorNode, new VectorImageUtils.SpriteOptions()
@@ -36,35 +34,7 @@ namespace Cdm.Figma.UI
 
         public override NodeObject Convert(NodeObject parentObject, Node node, NodeConvertArgs args)
         {
-            return Convert(parentObject, (VectorNode) node, args);
-        }
-
-        private static void SetPosition(VectorNode vectorNode, NodeObject vectorNodeObject)
-        {
-            var scale = vectorNode.relativeTransform.GetScale();
-            var offset = Vector2.Scale((Vector2) vectorNode.size * 0.5f, scale);
-            var position = vectorNode.relativeTransform.GetPosition();
-
-            vectorNodeObject.rectTransform.localPosition = new Vector3(position.x + offset.x, position.y - offset.y, 0);
-            vectorNodeObject.rectTransform.localScale = new Vector3(scale.x, scale.y, 1);
-        }
-
-        private static void SetSize(VectorNode vectorNode, NodeObject vectorNodeObject)
-        {
-            vectorNodeObject.rectTransform.sizeDelta = new Vector2(vectorNode.size.x, vectorNode.size.y);
-        }
-
-        private static void SetTransformOrigin(NodeObject nodeObject)
-        {
-            nodeObject.rectTransform.pivot = new Vector2(0.5f, 0.5f);
-        }
-
-        private static void SetRotation(Node node, NodeObject nodeObject)
-        {
-            VectorNode vectorNode = (VectorNode) node;
-            var relativeTransform = vectorNode.relativeTransform;
-            var rotation = relativeTransform.GetRotationAngle();
-            nodeObject.rectTransform.localRotation = relativeTransform.GetRotation();
+            return Convert(parentObject, (VectorNode)node, args);
         }
     }
 }
