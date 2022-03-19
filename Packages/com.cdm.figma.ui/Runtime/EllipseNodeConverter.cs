0bfff8162863bc016bcfa59a3f7bd5bd893768f2
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2022-03-19 17:04:57 +0300

    Fix ellipse node converter

--- Packages/com.cdm.figma.ui/Runtime/EllipseNodeConverter.cs
+++ Packages/com.cdm.figma.ui/Runtime/EllipseNodeConverter.cs
@@ -1,10 +1,10 @@
 ﻿namespace Cdm.Figma.UI
 {
-    public class EllipseNodeConverter : NodeConverter<StarNode>
+    public class EllipseNodeConverter : NodeConverter<EllipseNode>
     {
         public override NodeObject Convert(NodeObject parentObject, Node node, NodeConvertArgs args)
         {
-            return VectorNodeConverter.Convert(parentObject, (StarNode) node, args);
+            return VectorNodeConverter.Convert(parentObject, (EllipseNode) node, args);
         }
     }
 }
