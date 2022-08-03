ef355d2e03f9914ee1edbae368e283c0f7b9c318
gunseli <gns.ozkan@gmail.com>
2022-08-03 14:30:26 +0300

    Add Scrollview Component Converter

--- Packages/com.cdm.figma.ui/Runtime/FigmaImporter.cs
+++ Packages/com.cdm.figma.ui/Runtime/FigmaImporter.cs
@@ -49,7 +49,8 @@ namespace Cdm.Figma.UI
                 new ToggleComponentConverter(),
                 new SliderComponentConverter(),
                 new InputFieldComponentConverter(),
-                new ScrollbarComponentConverter()
+                new ScrollbarComponentConverter(),
+                new ScrollviewComponentConverter()
             };
         }
         
