4446cce8f5fb1fc93a9024bcc45cbe227a9e3170
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2022-08-16 17:20:16 +0300

    Remove legacy runtime figma importer

---
+++ Assets/Scripts/InstantiatePageNode.cs
@@ -0,0 +1,13 @@
+﻿using Cdm.Figma.UI;
+using UnityEngine;
+
+public class InstantiatePageNode : MonoBehaviour
+{
+    public Canvas canvas;
+    public PageNodeObject page;
+
+    private void Awake()
+    {
+        Instantiate(page, canvas.transform);
+    }
+}
