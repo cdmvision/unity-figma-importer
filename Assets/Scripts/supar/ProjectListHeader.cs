cfb243f2f4500d430eee5cae7491f5256c8294ba
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2022-12-16 16:34:17 +0300

    Add bindings

---
+++ Assets/Scripts/supar/ProjectListHeader.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Cdm.Figma.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Cdm.SurfaceProjector.UI
+{
+    [FigmaNode("ProjectListHeader")]
+    public class ProjectListHeader : MonoBehaviour, IFigmaNodeBinder
+    {
+        public enum SortingTarget
+        {
+            ByProject,
+            ByDate,
+            BySize
+        }
+
+        [SerializeField]
+        [FigmaNode("@ProjectSortItem", isRequired = true)]
+        private ProjectListHeaderItem _projectItem;
+        
+        [SerializeField]
+        [FigmaNode("@DateSortItem", isRequired = true)]
+        private ProjectListHeaderItem _dateItem;
+        
+        [SerializeField]
+        [FigmaNode("@SizeSortItem", isRequired = true)]
+        private ProjectListHeaderItem _sizeItem;
+
+        private ToggleGroup _toggleGroup;
+
+        public ProjectListHeaderItem.SortingMode GetSortingModeFor(SortingTarget sortingTarget)
+        {
+            switch (sortingTarget)
+            {
+                case SortingTarget.ByProject:
+                    return _projectItem.sortingMode;
+                case SortingTarget.ByDate:
+                    return _dateItem.sortingMode;
+                case SortingTarget.BySize:
+                    return _sizeItem.sortingMode;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(sortingTarget), sortingTarget, null);
+            }
+        }
+
+        public void OnBind(FigmaNode figmaNode)
+        {
+            var toggleGroup = figmaNode.gameObject.AddComponent<ToggleGroup>();
+            toggleGroup.allowSwitchOff = false;
+
+            _projectItem.mainToggle.group = toggleGroup;
+            _dateItem.mainToggle.group = toggleGroup;
+            _sizeItem.mainToggle.group = toggleGroup;
+        }
+        
+        protected virtual void OnSortingModeChanged(SortingModeChangedEventArgs e)
+        {
+            sortingModeChanged?.Invoke(e);
+        }
+
+        public event Action<SortingModeChangedEventArgs> sortingModeChanged;
+        
+        public readonly struct SortingModeChangedEventArgs
+        {
+            public SortingTarget sortingTarget { get; }
+            public ProjectListHeaderItem.SortingMode sortingMode { get; }
+
+            public SortingModeChangedEventArgs(SortingTarget sortingTarget, 
+                ProjectListHeaderItem.SortingMode sortingMode)
+            {
+                this.sortingTarget = sortingTarget;
+                this.sortingMode = sortingMode;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/supar/ProjectListHeaderItem.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Cdm.Figma.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Cdm.SurfaceProjector.UI
+{
+    [FigmaComponent("ProjectListHeaderItem")]
+    public class ProjectListHeaderItem : MonoBehaviour
+    {
+        public enum SortingMode
+        {
+            None,
+            Ascending,
+            Descending
+        }
+
+        [SerializeField]
+        [FigmaNode("@MainToggle", isRequired = true)]
+        private Toggle _mainToggle;
+
+        public Toggle mainToggle => _mainToggle;
+        
+        [SerializeField]
+        [FigmaNode("@SortToggle", isRequired = true)]
+        private Toggle _sortToggle;
+
+        [SerializeField]
+        private SortingMode _sortingMode = SortingMode.None;
+
+        public SortingMode sortingMode
+        {
+            get => _sortingMode;
+            set
+            {
+                _sortingMode = value;
+                UpdateSortingMode();
+            }
+        }
+
+        private void Awake()
+        {
+            if (Application.isPlaying)
+            {
+                sortingMode = _sortingMode;
+
+                _mainToggle.onValueChanged.AddListener(OnToggleValueChanged);
+                _sortToggle.onValueChanged.AddListener(OnToggleValueChanged);    
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (Application.isPlaying)
+            {
+                _mainToggle.onValueChanged.RemoveListener(OnToggleValueChanged);
+                _sortToggle.onValueChanged.RemoveListener(OnToggleValueChanged);
+            }
+        }
+
+        private void OnToggleValueChanged(bool isOn)
+        {
+            switch (sortingMode)
+            {
+                case SortingMode.None:
+                    sortingMode = SortingMode.Ascending;
+                    break;
+                case SortingMode.Ascending:
+                    sortingMode = SortingMode.Descending;
+                    break;
+                case SortingMode.Descending:
+                    sortingMode = SortingMode.Ascending;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void UpdateSortingMode()
+        {
+            _mainToggle.onValueChanged.RemoveListener(OnToggleValueChanged);
+            _mainToggle.isOn = sortingMode != SortingMode.None;
+            _mainToggle.onValueChanged.AddListener(OnToggleValueChanged);
+
+            _sortToggle.gameObject.SetActive(_mainToggle.isOn);
+
+            _sortToggle.onValueChanged.RemoveListener(OnToggleValueChanged);
+            _sortToggle.isOn = sortingMode == SortingMode.Descending;
+            _sortToggle.onValueChanged.AddListener(OnToggleValueChanged);
+        }
+
+        private void OnValidate()
+        {
+            if (Application.isPlaying)
+            {
+                sortingMode = _sortingMode;
+            }
+        }
+    }
+}
