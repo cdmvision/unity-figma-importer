b2ae33012ab84058c7c88e8ac1ed8356510c0d2e
H. Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2022-12-19 23:05:36 +0300

    Merge pull request #22 from cdmvision/develop
    1.1.1

---
+++ Runtime/EffectConverters/EffectConverter.cs
@@ -0,0 +1,40 @@
+﻿using Cdm.Figma.UI.Styles;
+
+namespace Cdm.Figma.UI
+{
+    public abstract class EffectConverter : IEffectConverter
+    {
+        public abstract bool CanConvert(FigmaNode node, Effect effect);
+        public abstract void Convert(FigmaNode node, Effect effect);
+    }
+    
+    public abstract class EffectConverter<TEffectType, TStyle, TStyleSetter, TBehaviour> : EffectConverter 
+        where TEffectType : Effect
+        where TStyleSetter : StyleSetterWithSelectorsBase
+        where TStyle : EffectStyle<TStyleSetter>
+        where TBehaviour: EffectBehaviour
+    {
+        public override bool CanConvert(FigmaNode node, Effect effect)
+        {
+            return effect is TEffectType;
+        }
+
+        public override void Convert(FigmaNode node, Effect effect)
+        {
+            var style = CreateStyle(node, (TEffectType)effect);
+            if (style != null)
+            {
+                var effectId = node.GetComponents<TBehaviour>().Length;
+                
+                style.effectId = effectId;
+                node.styles.Add(style);
+                
+                var effectBehaviour = Convert(node, style);
+                effectBehaviour.effectId = effectId;
+            }
+        }
+
+        protected abstract TStyle CreateStyle(FigmaNode node, TEffectType effect);
+        protected abstract TBehaviour Convert(FigmaNode node, TStyle style);
+    }
+}
---
+++ Runtime/NodeConverters/InstanceNodeConverter.cs
@@ -0,0 +1,36 @@
+﻿using Cdm.Figma.UI.Utils;
+using UnityEngine;
+
+namespace Cdm.Figma.UI
+{
+    public class InstanceNodeConverter : NodeConverter<InstanceNode>
+    {
+        protected override FigmaNode Convert(FigmaNode parentObject, InstanceNode node, NodeConvertArgs args)
+        {
+            var frameNodeConverter = new FrameNodeConverter();
+
+            // Use instance node's transform.
+            var instanceNode = frameNodeConverter.Convert(parentObject, node, args);
+
+            var propertyReference = node.componentPropertyReferences?.mainComponent;
+            if (!string.IsNullOrEmpty(propertyReference))
+            {
+                if (args.componentPropertyAssignments.TryGetValue(propertyReference, out var componentNode))
+                {
+                    var componentInstance = frameNodeConverter.Convert(parentObject, componentNode, args);
+                    componentInstance.referenceNode = node;
+                    
+                    instanceNode.rectTransform.CopyTo(componentInstance.rectTransform);
+
+                    args.importer.DestroyFigmaNode(instanceNode);
+                    return componentInstance;
+                }
+
+                Debug.LogWarning(
+                    $"Instance node {node} instance swap property reference could not found: {propertyReference}");
+            }
+
+            return instanceNode;
+        }
+    }
+}
