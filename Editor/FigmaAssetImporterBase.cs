339755bfe016801965794438ad1392fad1e1140b
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2023-03-24 03:42:33 +0300

    Merge branch 'develop'

---
+++ Editor/FigmaAssetImporterBase.cs
@@ -0,0 +1,120 @@
+using System;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.AssetImporters;
+using UnityEngine;
+
+namespace Cdm.Figma.Editor
+{
+    public abstract class FigmaAssetImporterBase : ScriptedImporter
+    {
+        protected const string DefaultExtension = "figma";
+
+        protected const string FigmaIconColorPath = EditorHelper.PackageFolderPath +
+                                                    "/Editor Default Resources/FigmaIcon-Color.png";
+
+        protected const string FigmaIconFlatPath = EditorHelper.PackageFolderPath +
+                                                   "/Editor Default Resources/FigmaIcon-Flat.png";
+
+        [SerializeField]
+        private FigmaFilePage[] _pages;
+
+        /// <summary>
+        /// Selected pages to be imported.
+        /// </summary>
+        public FigmaFilePage[] pages
+        {
+            get => _pages;
+            set => _pages = value;
+        }
+
+        public override void OnImportAsset(AssetImportContext ctx)
+        {
+            var fileJson = File.ReadAllText(ctx.assetPath);
+            var figmaFile = FigmaFile.Parse(fileJson);
+
+            UpdatePages(figmaFile);
+
+            var figmaImporter = GetFigmaImporter();
+            OnAssetImporting(ctx, figmaImporter, figmaFile);
+
+            var figmaDesign = figmaImporter.ImportFile(figmaFile, new IFigmaImporter.Options()
+            {
+                selectedPages = _pages.Where(p => p.enabled).Select(p => p.id).ToArray()
+            });
+
+            OnAssetImported(ctx, figmaImporter, figmaFile, figmaDesign);
+
+            if (figmaDesign.thumbnail != null)
+            {
+                ctx.AddObjectToAsset("FigmaDesignPreview", figmaDesign.thumbnail);
+            }
+
+            var icon = AssetDatabase.LoadAssetAtPath<Texture2D>(FigmaIconColorPath);
+            ctx.AddObjectToAsset("FigmaDesign", figmaDesign.gameObject, icon);
+            ctx.SetMainObject(figmaDesign.gameObject);
+        }
+
+        private void UpdatePages(FigmaFile file)
+        {
+            if (importSettingsMissing)
+            {
+                _pages = null;
+            }
+
+            var newPages = file.document.children ?? Array.Empty<PageNode>();
+            newPages = newPages.Where(x => !x.IsIgnored()).ToArray();
+
+            var oldPages = _pages;
+            _pages = new FigmaFilePage[newPages.Length];
+
+            for (var i = 0; i < _pages.Length; i++)
+            {
+                _pages[i] = new FigmaFilePage(newPages[i].id, newPages[i].name);
+
+                // Restore previously page status.
+                if (oldPages != null)
+                {
+                    var oldPageIndex = Array.FindIndex(oldPages, x => x.id == _pages[i].id);
+                    if (oldPageIndex >= 0)
+                    {
+                        _pages[i].enabled = oldPages[oldPageIndex].enabled;
+                    }
+                }
+            }
+        }
+
+        protected virtual void OnAssetImporting(AssetImportContext ctx, IFigmaImporter figmaImporter,
+            FigmaFile figmaFile)
+        {
+        }
+
+        protected virtual void OnAssetImported(AssetImportContext ctx, IFigmaImporter figmaImporter,
+            FigmaFile figmaFile, FigmaDesign figmaDesign)
+        {
+        }
+
+        protected virtual string GetAssetExtension()
+        {
+            return DefaultExtension;
+        }
+
+        protected abstract IFigmaImporter GetFigmaImporter();
+    }
+
+    [Serializable]
+    public struct FigmaFilePage
+    {
+        public bool enabled;
+        public string id;
+        public string name;
+
+        public FigmaFilePage(string id, string name)
+        {
+            this.id = id;
+            this.name = name;
+            this.enabled = true;
+        }
+    }
+}
---
+++ Runtime/Figma/Nodes/PageNode.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace Cdm.Figma
+{
+    /// <summary>
+    /// The page node is always a descendent of the <see cref="DocumentNode"/>. 
+    /// </summary>
+    [DataContract]
+    public class PageNode : Node
+    {
+        public override string type => NodeType.Page;
+        
+        /// <summary>
+        /// A list of top level layers on the canvas.
+        /// </summary>
+        [DataMember(Name = "children")]
+        public SceneNode[] children { get; set; }
+        
+        /// <summary>
+        /// Background color of the canvas.
+        /// </summary>
+        [DataMember(Name = "backgroundColor")]
+        public Color backgroundColor { get; set; }
+
+        /// <summary>
+        /// A array of flow starting points sorted by its position in the prototype settings panel.
+        /// </summary>
+        [DataMember(Name = "flowStartingPoints")]
+        public List<FlowStartingPoint> flowStartingPoints { get; private set; } = new List<FlowStartingPoint>();
+        
+        /// <summary>
+        /// A list of export settings representing images to export from the canvas.
+        /// </summary>
+        [DataMember(Name = "exportSettings")]
+        public List<ExportSetting> exportSettings { get; private set; } = new List<ExportSetting>();
+
+        public override Node[] GetChildren() => children.Cast<Node>().ToArray();
+    }
+
+    /// <summary>
+    /// A flow starting point used when launching a prototype to enter Presentation view.
+    /// </summary>
+    [DataContract]
+    public class FlowStartingPoint
+    {
+        /// <summary>
+        /// Unique identifier specifying the frame.
+        /// </summary>
+        [DataMember(Name = "nodeId")]
+        public string nodeId { get; set; }
+        
+        /// <summary>
+        /// Unique identifier specifying the frame.
+        /// </summary>
+        [DataMember(Name = "name")]
+        public string name { get; set; }
+    }
+}
