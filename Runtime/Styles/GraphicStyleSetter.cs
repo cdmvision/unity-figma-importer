98123d38b0f914aee4021da69414564ffb15c5b1
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2023-04-22 04:39:12 +0300

    Merge branch 'develop'

---
+++ Runtime/Styles/GraphicStyleSetter.cs
@@ -0,0 +1,19 @@
+﻿using Cdm.Figma.UI.Styles.Properties;
+using UnityEngine.UI;
+
+namespace Cdm.Figma.UI.Styles
+{
+    public class GraphicStyleSetter : StyleSetterWithSelectors<GraphicStyle>
+    {
+    }
+    
+    public static class GraphicExtensions
+    {
+        public static void CrossFadeColor(this Graphic graphic, StyleArgs args, 
+            StylePropertyColor color, StylePropertyFloat fadeDuration)
+        {
+            var duration = !fadeDuration.enabled || args.instant ? 0f : fadeDuration.value;
+            graphic.CrossFadeColor(color.value, duration, false, true);    
+        }
+    }
+}
