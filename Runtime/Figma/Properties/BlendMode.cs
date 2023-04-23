59bd54b75c3c61eae87c1d2984957bf1f4ee2308
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2023-04-23 18:43:54 +0300

    Merge branch 'develop'

---
+++ Runtime/Figma/Properties/BlendMode.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace Cdm.Figma
+{
+    /// <summary>
+    /// Blend mode describes how a color blends with what's underneath it. This property is typically set on a layer,
+    /// fill or effect (e.g. blend mode of the shadow).
+    ///
+    /// These blend modes are fairly standard and should match what you would find in other image processing tools.
+    /// </summary>
+    /// <seealso aref="https://developer.mozilla.org/en-US/docs/Web/CSS/blend-mode"/>
+    [DataContract]
+    [Serializable]
+    public enum BlendMode
+    {
+        [EnumMember(Value = "COLOR")]
+        Color,
+
+        [EnumMember(Value = "COLOR_BURN")]
+        ColorBurn,
+
+        [EnumMember(Value = "COLOR_DODGE")]
+        ColorDodge,
+
+        [EnumMember(Value = "DARKEN")]
+        Darken,
+
+        [EnumMember(Value = "DIFFERENCE")]
+        Difference,
+
+        [EnumMember(Value = "EXCLUSION")]
+        Exclusion,
+
+        [EnumMember(Value = "HARD_LIGHT")]
+        HardLight,
+
+        [EnumMember(Value = "HUE")]
+        Hue,
+
+        [EnumMember(Value = "LIGHTEN")]
+        Lighten,
+
+        [EnumMember(Value = "LINEAR_BURN")]
+        LinearBurn,
+
+        [EnumMember(Value = "LINEAR_DODGE")]
+        LinearDodge,
+
+        [EnumMember(Value = "LUMINOSITY")]
+        Luminosity,
+
+        [EnumMember(Value = "MULTIPLY")]
+        Multiply,
+
+        [EnumMember(Value = "NORMAL")]
+        Normal,
+
+        [EnumMember(Value = "OVERLAY")]
+        Overlay,
+
+        [EnumMember(Value = "PASS_THROUGH")]
+        PassThrough,
+
+        [EnumMember(Value = "SATURATION")]
+        Saturation,
+
+        [EnumMember(Value = "SCREEN")]
+        Screen,
+
+        [EnumMember(Value = "SOFT_LIGHT")]
+        SoftLight
+    }
+}
