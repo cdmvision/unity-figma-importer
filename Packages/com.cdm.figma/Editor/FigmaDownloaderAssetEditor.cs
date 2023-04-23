168c1b35260840fd7ed0ee1f5193882ce1f2a1f1
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2023-04-23 06:24:26 +0300

    Fix figma downloader file name inspector

--- Packages/com.cdm.figma/Editor/FigmaDownloaderAssetEditor.cs
+++ Packages/com.cdm.figma/Editor/FigmaDownloaderAssetEditor.cs
@@ -1,12 +1,10 @@
 using System;
-using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEditor;
-using UnityEditorInternal;
 using UnityEngine;
 using CompressionLevel = System.IO.Compression.CompressionLevel;
 using Debug = UnityEngine.Debug;
@@ -24,9 +22,7 @@ namespace Cdm.Figma.Editor
         private SerializedProperty _fileId;
         private SerializedProperty _fileVersion;
         private SerializedProperty _fileName;
-        private SerializedProperty _fileDefaultName;
-
-        private ReorderableList _fileList;
+        private SerializedProperty _defaultFileName;
 
         private bool _isDownloading = false;
         private bool _isDownloadingCompleted = false;
@@ -45,7 +41,7 @@ namespace Cdm.Figma.Editor
             _fileId = serializedObject.FindProperty("_fileId");
             _fileVersion = serializedObject.FindProperty("_fileVersion");
             _fileName = serializedObject.FindProperty("_fileName");
-            _fileDefaultName = serializedObject.FindProperty("_fileDefaultName");
+            _defaultFileName = serializedObject.FindProperty("_defaultFileName");
         }
 
         public override bool RequiresConstantRepaint()
@@ -77,7 +73,7 @@ namespace Cdm.Figma.Editor
             EditorGUILayout.Separator();
             EditorGUILayout.Separator();
             EditorGUILayout.PropertyField(_fileId, new GUIContent("File ID"));
-            EditorGUILayout.PropertyField(_fileName);
+            DrawFileName();
             EditorGUILayout.PropertyField(_fileVersion);
 
             EditorGUILayout.Separator();
@@ -124,56 +120,28 @@ namespace Cdm.Figma.Editor
             serializedObject.ApplyModifiedProperties();
         }
         
-        private void DrawFileFields()
+        private void DrawFileName()
         {
-            EditorGUILayout.PropertyField(_fileId);
-            EditorGUILayout.PropertyField(_fileName);    
+            var rect = EditorGUILayout.GetControlRect(true);
+            EditorGUI.PropertyField(rect, _fileName);
             
-            /*if (!string.IsNullOrEmpty(_fileName.stringValue))
+            if (string.IsNullOrEmpty(_fileName.stringValue))
             {
-                EditorGUILayout.PropertyField(_fileName);    
-            }
-            else
-            {
+                var text = !string.IsNullOrEmpty(_defaultFileName.stringValue)
+                    ? _defaultFileName.stringValue
+                    : "File name (optional)";
                 
-            }*/
+                Placeholder(new Rect(
+                    rect.x + EditorGUIUtility.labelWidth + 4, rect.y, 
+                    rect.width - 4 - EditorGUIUtility.labelWidth, rect.height), text);
+            }
         }
 
-        /*private void DrawElement(Rect rect, int index, bool isActive, bool isFocused)
-        {
-            var file = _fileList.serializedProperty.GetArrayElementAtIndex(index);
-            var fileId = file.FindPropertyRelative("id");
-            var fileName = file.FindPropertyRelative("name");
-            var defaultName = file.FindPropertyRelative("defaultName");
-
-            var idRect = new Rect(rect.x, rect.y + 2, rect.width * 0.5f, EditorGUIUtility.singleLineHeight);
-
-            EditorGUI.PropertyField(idRect, fileId, GUIContent.none);
-
-            if (string.IsNullOrEmpty(fileId.stringValue))
-            {
-                Placeholder(new Rect(idRect.x + 2, idRect.y, idRect.width - 2, idRect.height), "File ID");
-            }
-
-            var nameRect = new Rect(idRect.x + idRect.width + 2,
-                rect.y + 2, rect.width - (idRect.width - 2), EditorGUIUtility.singleLineHeight);
-
-            EditorGUI.PropertyField(nameRect, fileName, GUIContent.none);
-
-            if (string.IsNullOrEmpty(fileName.stringValue))
-            {
-                var text = !string.IsNullOrEmpty(defaultName.stringValue)
-                    ? defaultName.stringValue
-                    : "File name (optional)";
-                Placeholder(new Rect(nameRect.x + 2, nameRect.y, nameRect.width - 2, nameRect.height), text);
-            }
-        }*/
-
-        private static void Placeholder(string value)
+        private static void Placeholder(Rect rect, string value)
         {
             var guiColor = GUI.color;
             GUI.color = UnityEngine.Color.grey;
-            EditorGUILayout.LabelField(value);
+            EditorGUI.LabelField(rect, value);
             GUI.color = guiColor;
         }
 
