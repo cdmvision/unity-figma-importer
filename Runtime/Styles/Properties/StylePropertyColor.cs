14df8f901a46b4185d1bd2417e729c887fe438ab
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2023-04-25 23:34:19 +0300

    Merge branch 'develop'

---
+++ Runtime/Styles/Properties/StylePropertyColor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Cdm.Figma.UI.Styles.Properties
+{
+    [Serializable]
+    public class StylePropertyColor : StyleProperty<UnityEngine.Color>
+    {
+        public StylePropertyColor()
+        {
+        }
+
+        public StylePropertyColor(UnityEngine.Color defaultValue) : base(defaultValue)
+        {
+        }
+    }
+}
