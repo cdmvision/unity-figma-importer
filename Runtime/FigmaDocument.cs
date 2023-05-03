4805699383e29c5bf76a08bb04c7b0d0f7d41060
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2023-05-03 13:23:36 +0300

    Merge branch 'develop'

---
+++ Runtime/FigmaDocument.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Cdm.Figma.UI
+{    
+    [SelectionBase]
+    public class FigmaDocument : FigmaNode
+    {
+        [SerializeField]
+        private List<FigmaPage> _pages = new List<FigmaPage>();
+
+        public IList<FigmaPage> pages => _pages;
+        
+        public static FigmaDocument Instantiate(FigmaDocument original, Transform parent = null)
+        {
+            var document = Object.Instantiate(original, parent);
+            
+            document._pages.Clear();
+            foreach (var page in original.pages)
+            {
+                document._pages.Add(Object.Instantiate(page, document.transform));
+            }
+            
+            return document;
+        }
+
+#if UNITY_EDITOR
+        public static FigmaDocument InstantiatePrefab(FigmaDocument original, Transform parent = null)
+        {
+            var documentGo = (GameObject)UnityEditor.PrefabUtility.InstantiatePrefab(original.gameObject, parent);
+            var document = documentGo.GetComponent<FigmaDocument>();
+            
+            document._pages.Clear();
+            foreach (var page in original.pages)
+            {
+                var pageGo = (GameObject) UnityEditor.PrefabUtility.InstantiatePrefab(page.gameObject, document.transform);
+                document._pages.Add(pageGo.GetComponent<FigmaPage>());
+            }
+            
+            return document;
+        }
+#else
+        public static FigmaDocument InstantiatePrefab(FigmaDocument original, Transform parent = null)
+        {
+            throw new NotImplementedException();
+        }
+#endif
+        
+
+        public IEnumerable<FigmaImporterLogReference> GetLogs()
+        {
+            var allLogs = Enumerable.Empty<FigmaImporterLogReference>();
+
+            foreach (var page in pages)
+            {
+                allLogs = allLogs.Concat(page.allLogs);
+            }
+            
+            return allLogs;
+        }
+    }
+}
