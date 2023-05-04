37478b3dc8c21b82bfa7190618a77f5633a64f03
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2023-05-04 19:24:43 +0300

    Merge branch 'develop'

---
+++ Runtime/Integrations/UnityLocalization/UnityLocalizationConverter.cs
@@ -0,0 +1,36 @@
+﻿using Cdm.Figma.UI.Styles;
+using UnityEngine;
+using UnityEngine.Localization;
+
+namespace Cdm.Figma.UI
+{
+    public class UnityLocalizationConverter : ILocalizationConverter
+    {
+        public bool CanConvert(FigmaText node)
+        {
+            return !string.IsNullOrEmpty(node.localizationKey);
+        }
+
+        public void Convert(FigmaText node)
+        {
+            var style = new UnityLocalizationStyle();
+            style.localizedString.enabled = false;
+
+            var localizationKey = node.localizationKey;
+            if (!string.IsNullOrEmpty(localizationKey))
+            {
+                if (UnityLocalizationHelper.TryGetTableAndEntryReference(
+                        localizationKey, out var tableReference, out var tableEntryReference))
+                {
+                    style.localizedString.SetValue(new LocalizedString(tableReference, tableEntryReference));
+                }
+                else
+                {
+                    Debug.LogWarning($"Localization key could not be mapped: {localizationKey}");
+                }
+            }
+
+            node.styles.Add(style);
+        }
+    }
+}
---
+++ Runtime/NodeConverters/VectorNodeConverter.cs
@@ -0,0 +1,130 @@
+using System.Collections.Generic;
+using System.Linq;
+using Cdm.Figma.UI.Styles;
+using Cdm.Figma.UI.Utils;
+using Cdm.Figma.Utils;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Cdm.Figma.UI
+{
+    public class VectorConvertArgs
+    {
+        public Sprite sourceSprite { get; set; }
+        public bool generateSprite { get; set; } = true;
+    }
+
+    public abstract class VectorNodeConverter<TNode, TFigmaNode> : NodeConverter<TNode>
+        where TNode : VectorNode
+        where TFigmaNode : FigmaNode
+    {
+        protected override FigmaNode Convert(FigmaNode parentObject, TNode vectorNode, NodeConvertArgs args)
+        {
+            var figmaNode = Convert(parentObject, vectorNode, args, new VectorConvertArgs());
+            if (figmaNode != null && vectorNode.isMask)
+            {
+                args.importer.LogWarning("Vector node with mask is not supported.", figmaNode);
+            }
+
+            return figmaNode;
+        }
+
+        protected FigmaNode Convert(FigmaNode parentObject, TNode vectorNode, NodeConvertArgs args,
+            VectorConvertArgs vectorConvertArgs)
+        {
+            var figmaNode = args.importer.CreateFigmaNode<TFigmaNode>(vectorNode);
+            figmaNode.SetTransform(vectorNode);
+
+            // Every vector's parent will ALWAYS be INodeTransform
+            figmaNode.SetLayoutConstraints((INodeTransform)vectorNode.parent);
+
+            GenerateStyles(figmaNode, vectorNode, args, vectorConvertArgs);
+
+            figmaNode.ApplyStyles();
+            return figmaNode;
+        }
+
+        private static void GenerateStyles(FigmaNode nodeObject, TNode vectorNode, NodeConvertArgs args,
+            VectorConvertArgs vectorConvertArgs)
+        {
+            if (vectorConvertArgs.generateSprite)
+            {
+                var sprite = vectorConvertArgs.sourceSprite;
+                if (sprite == null)
+                {
+                    sprite = GenerateSprite(vectorNode, SpriteGenerateType.Path, args);
+                    
+                    if (sprite != null)
+                    {
+                        vectorConvertArgs.sourceSprite = sprite;
+                    }
+                }
+
+                var style = new ImageStyle();
+                style.componentEnabled.enabled = true;
+                style.componentEnabled.value = vectorConvertArgs.sourceSprite != null;
+
+                style.sprite.enabled = true;
+                style.sprite.value = vectorConvertArgs.sourceSprite;
+
+                style.imageType.enabled = true;
+                style.imageType.value = sprite.GetImageType();
+                nodeObject.styles.Add(style);
+            }
+
+            {
+                var style = new CanvasGroupStyle();
+                style.alpha.enabled = true;
+                style.alpha.value = vectorNode.opacity;
+                nodeObject.styles.Add(style);
+            }
+
+            args.importer.ConvertEffects(nodeObject, vectorNode.effects);
+        }
+        
+        public static Sprite GenerateSprite(SceneNode node, SpriteGenerateType generateType, NodeConvertArgs args)
+        {
+            if (node is not INodeFill)
+                return null;
+            
+            var nodeId = node.id;
+            var spriteOptions = args.importer.spriteOptions;
+            
+            var fills = ((INodeFill)node).fills;
+            var strokes = ((INodeFill)node).strokes;
+            
+            if (args.overrideNode is INodeFill overrideNodeFill and SceneNode overrideSceneNode)
+            {
+                nodeId = $"{node.id}_{args.overrideNode.id}";
+                spriteOptions.overrideNode = overrideSceneNode;
+                
+                fills = overrideNodeFill.fills;
+                strokes = overrideNodeFill.strokes;
+            }
+            
+            if ((fills != null && fills.Any()) || (strokes != null && strokes.Any()))
+            {
+                if (!args.importer.generatedAssets.TryGet<Sprite>(nodeId, out var sprite))
+                {
+                    sprite = NodeSpriteGenerator.GenerateSprite(args.file, node, generateType, spriteOptions);
+
+                    if (sprite != null)
+                    {
+                        sprite.name = nodeId;
+                        args.importer.generatedAssets.Add(nodeId, sprite);
+                        args.importer.generatedAssets.Add(nodeId, sprite.texture);
+                        return sprite;
+                    }
+                }
+
+                return sprite;
+            }
+
+            return null;
+        }
+    }
+
+    public class VectorNodeConverter : VectorNodeConverter<VectorNode, FigmaNode>
+    {
+    }
+}
