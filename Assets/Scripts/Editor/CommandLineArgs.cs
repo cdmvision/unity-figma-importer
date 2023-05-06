3f375523174615d7078084a75d41cb5bf5027a52
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2023-05-06 18:14:40 +0300

    Minor changes

--- Assets/Scripts/Editor/CommandLineArgs.cs
+++ Assets/Scripts/Editor/CommandLineArgs.cs
@@ -7,6 +7,7 @@ public class CommandLineArgs
     [InitializeOnLoadMethod]
     public static void PrintArgs()
     {
+        // Print command line arguments for detection of error in Unity cloud build.
         var args = string.Join(" ", System.Environment.GetCommandLineArgs());
         Debug.Log($"Editor args: {args}");
     }
