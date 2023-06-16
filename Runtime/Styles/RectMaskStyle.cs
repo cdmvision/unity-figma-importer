dab46fedb7fa95b159b2edf9d0ce5679ede05d82
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2023-06-16 22:50:58 +0300

    Merge branch 'develop'

---
+++ Runtime/Styles/RectMaskStyle.cs
@@ -0,0 +1,24 @@
+using System;
+using Cdm.Figma.UI.Utils;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Cdm.Figma.UI.Styles
+{
+    [Serializable]
+    public class RectMaskStyle : Style
+    {
+        public override void SetStyleAsSelector(GameObject gameObject, StyleArgs args)
+        {
+        }
+
+        protected override void MergeTo(Style other, bool force)
+        {
+        }
+
+        public override void SetStyle(GameObject gameObject, StyleArgs args)
+        {
+            gameObject.GetOrAddComponent<RectMask2D>();
+        }
+    }
+}
