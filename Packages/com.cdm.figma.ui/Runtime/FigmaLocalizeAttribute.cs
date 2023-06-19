7390ccfc900d58f0f9aef231458af7cc7694f447
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2023-06-19 22:22:39 +0300

    Fix #55

--- Packages/com.cdm.figma.ui/Runtime/FigmaLocalizeAttribute.cs
+++ Packages/com.cdm.figma.ui/Runtime/FigmaLocalizeAttribute.cs
@@ -1,9 +1,16 @@
-﻿namespace Cdm.Figma.UI
+﻿using System;
+
+namespace Cdm.Figma.UI
 {
-    public class FigmaLocalizeAttribute : FigmaImporterAttribute
+    public class FigmaLocalizeAttribute : Attribute
     {
         public string localizationKey { get; }
 
+        /// <summary>
+        /// The binding of field or property is whether required.
+        /// </summary>
+        public bool isRequired { get; set; } = true;
+        
         public FigmaLocalizeAttribute(string localizationKey)
         {
             this.localizationKey = localizationKey;
--- Packages/com.cdm.figma.ui/Runtime/FigmaResourceAttribute.cs
+++ Packages/com.cdm.figma.ui/Runtime/FigmaResourceAttribute.cs
@@ -5,12 +5,16 @@ namespace Cdm.Figma.UI
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
     public class FigmaResourceAttribute : Attribute
     {
+        /// <summary>
+        /// Path of the asset stored in a Resources folder.
+        /// </summary>
+        /// <seealso href="https://docs.unity3d.com/ScriptReference/Resources.html"/>
         public string path { get; }
         
         /// <summary>
         /// The binding of field or property is whether required.
         /// </summary>
-        public bool isRequired { get; set; }
+        public bool isRequired { get; set; } = true;
 
         public FigmaResourceAttribute(string path)
         {
