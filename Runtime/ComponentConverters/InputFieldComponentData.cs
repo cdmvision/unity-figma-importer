fb6cfaf4a0545eacbff42cc2be502f910a8abd03
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2023-06-19 22:22:49 +0300

    Merge branch 'develop'

---
+++ Runtime/ComponentConverters/InputFieldComponentData.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+using Cdm.Figma.Utils;
+using Newtonsoft.Json;
+
+namespace Cdm.Figma.UI
+{
+    [DataContract]
+    public class InputFieldComponentData
+    {
+        [DataMember]
+        [JsonConverter(typeof(ColorHexConverter))]
+        public Color selectionColor { get; set; }
+
+        [DataMember]
+        public int selectionColorOpacity { get; set; } = 75;
+        
+        [DataMember]
+        [JsonConverter(typeof(ColorHexConverter))]
+        public Color caretColor { get; set; }
+
+        [DataMember]
+        public int caretColorOpacity { get; set; } = 100;
+
+        [DataMember]
+        public int caretWidth { get; set; } = 1;
+    }
+}
