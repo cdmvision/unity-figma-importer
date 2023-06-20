cac03a04132c26b4296254aaf1e329cd03e5da08
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2023-06-20 15:50:47 +0300

    Merge branch 'develop'

---
+++ Runtime/Utils/SpriteExtensions.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Cdm.Figma.Utils;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Cdm.Figma.UI.Utils
+{
+    public static class SpriteExtensions
+    {
+        public static Image.Type GetImageType(this Sprite sprite)
+        {
+            if (sprite == null)
+                return Image.Type.Simple;
+            
+            return sprite.IsSliced() ? Image.Type.Sliced : Image.Type.Simple;
+        }
+    }
+}
