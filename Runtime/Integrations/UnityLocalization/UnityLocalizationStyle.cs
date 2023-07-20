adc4be06fec0cba3b3c86cc1795e2e26c31fbc70
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2023-07-20 12:07:59 +0300

    Merge branch 'develop'

---
+++ Runtime/Integrations/UnityLocalization/UnityLocalizationStyle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Cdm.Figma.UI.Styles;
+using Cdm.Figma.UI.Styles.Properties;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Localization.Components;
+
+namespace Cdm.Figma.UI
+{
+    [Serializable]
+    public class UnityLocalizationStyle : StyleWithSetter<UnityLocalizationStyleSetter>
+    {
+        public StylePropertyLocalizedString localizedString = new StylePropertyLocalizedString();
+
+        protected override void MergeTo(Style other, bool force)
+        {
+            if (other is UnityLocalizationStyle otherStyle)
+            {
+                OverwriteProperty(localizedString, otherStyle.localizedString, force);
+            }
+        }
+
+        public override void SetStyle(GameObject gameObject, StyleArgs args)
+        {
+            var textComponent = gameObject.GetComponent<TMP_Text>();
+            if (textComponent != null && localizedString.enabled)
+            {
+                var stringEvent = gameObject.GetComponent<LocalizeStringEvent>();
+
+                if (stringEvent == null)
+                {
+                    stringEvent = gameObject.AddComponent<LocalizeStringEvent>();
+                    UnityLocalizationHelper.AddUpdateStringEvent(textComponent, stringEvent);
+                }
+
+                stringEvent.StringReference = localizedString.value;
+
+                if (Application.isPlaying)
+                {
+                    stringEvent.RefreshString();
+                }
+            }
+        }
+    }
+}
---
+++ Runtime/Styles/Properties/StylePropertyFont.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TMPro;
+
+namespace Cdm.Figma.UI.Styles.Properties
+{
+    [Serializable]
+    public class StylePropertyFont : StyleProperty<TMP_FontAsset>
+    {
+        public StylePropertyFont()
+        {
+        }
+
+        public StylePropertyFont(TMP_FontAsset defaultValue) : base(defaultValue)
+        {
+        }
+    }
+}
