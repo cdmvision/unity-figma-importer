5dbcfbb883eba8c491fd3ae61c6e1e6a941cb69e
H.Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2023-08-01 18:20:01 +0300

    Merge branch 'develop'

---
+++ Runtime/Utils/FigmaBindingFailedException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cdm.Figma.UI.Utils
+{
+    public class FigmaBindingFailedException : Exception
+    {
+        public IList<BindingError> errors { get; }
+
+        public FigmaBindingFailedException(IList<BindingError> errors)
+            : base("Binding of some figma nodes failed.")
+        {
+            this.errors = errors;
+        }
+
+        public FigmaBindingFailedException(IList<BindingError> errors, string message)
+            : base(message)
+        {
+            this.errors = errors;
+        }
+
+        public FigmaBindingFailedException(IList<BindingError> errors, string message, Exception inner)
+            : base(message, inner)
+        {
+            this.errors = errors;
+        }
+    }
+}
