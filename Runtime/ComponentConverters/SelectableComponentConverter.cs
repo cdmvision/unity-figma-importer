dd99ee4699bfde56dfb6155bf36b0d3a5135bf8c
H. Ibrahim Penekli <h.ibrahimpenekli@gmail.com>
2024-04-12 16:49:51 +0300

    Merge branch 'develop'

---
+++ Runtime/ComponentConverters/SelectableComponentConverter.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine.UI;
+
+namespace Cdm.Figma.UI
+{
+    public class SelectableComponentConverter :
+        SelectableComponentConverter<Selectable, SelectableComponentVariantFilter>
+    {
+        protected override bool CanConvertType(string typeId)
+        {
+            return typeId == "Selectable";
+        }
+    }
+
+    public abstract class SelectableComponentConverter<TComponent, TComponentVariantFilter> :
+        ComponentConverterWithVariants<TComponent, TComponentVariantFilter>
+        where TComponent : Selectable
+        where TComponentVariantFilter : SelectableComponentVariantFilter
+    {
+        private bool isSelectable => variants.ContainsKey(ComponentPropertySelected.Key);
+
+        protected override FigmaNode Convert(FigmaNode parentObject, InstanceNode instanceNode, NodeConvertArgs args)
+        {
+            var nodeObject = base.Convert(parentObject, instanceNode, args);
+            var variantFilter = nodeObject.GetComponent<TComponentVariantFilter>();
+            variantFilter.isSelectable = isSelectable;
+            return nodeObject;
+        }
+
+        protected override bool TryGetSelector(string[] variant, out string selector)
+        {
+            selector = "";
+
+            if (!TryGetSelector(variant, ComponentPropertyState.Default, ref selector) &&
+                !TryGetSelector(variant, ComponentPropertyState.Hover, ref selector) &&
+                !TryGetSelector(variant, ComponentPropertyState.Press, ref selector) &&
+                !TryGetSelector(variant, ComponentPropertyState.Disabled, ref selector))
+            {
+                return false;
+            }
+
+            if (isSelectable)
+            {
+                if (!TryGetSelector(variant, ComponentPropertySelected.Off, ref selector) &&
+                    !TryGetSelector(variant, ComponentPropertySelected.On, ref selector))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    public static class ComponentPropertyState
+    {
+        public const string Key = "State";
+        public static readonly ComponentProperty Default = new ComponentProperty(Key, "Default");
+        public static readonly ComponentProperty Hover = new ComponentProperty(Key, "Hover");
+        public static readonly ComponentProperty Press = new ComponentProperty(Key, "Press");
+        public static readonly ComponentProperty Disabled = new ComponentProperty(Key, "Disabled");
+    }
+
+    public static class ComponentPropertySelected
+    {
+        public const string Key = "Selected";
+        public static readonly ComponentProperty Off = new ComponentProperty(Key, "Off");
+        public static readonly ComponentProperty On = new ComponentProperty(Key, "On");
+    }
+}
---
+++ Runtime/Styles/Properties/StylePropertyImageType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine.UI;
+
+namespace Cdm.Figma.UI.Styles.Properties
+{
+    [Serializable]
+    public class StylePropertyImageType : StyleProperty<Image.Type>
+    {
+        public StylePropertyImageType()
+        {
+        }
+
+        public StylePropertyImageType(Image.Type defaultValue) : base(defaultValue)
+        {
+        }
+    }
+}
